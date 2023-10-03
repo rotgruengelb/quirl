85e5109db255beb2803b02497d7174ac744f3dab
rotgruengelb <rotgrungelblab@gmail.com>
2023-10-03 22:27:32 +0200

    Cleanup

--- src/main/java/net/rotgruengelb/quirl/Quirl.java
+++ src/main/java/net/rotgruengelb/quirl/Quirl.java
@@ -1,7 +1,6 @@
 package net.rotgruengelb.quirl;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.block.Blocks;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -11,12 +10,12 @@ import org.slf4j.LoggerFactory;
 
 public class Quirl implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("quirl");
-	public static final String MOD_ID = "quirl";
+    public static final String MOD_ID = "quirl";
 
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
-		CustomShovelInteract.addResult(Blocks.ACACIA_PLANKS.getDefaultState(), Blocks.DIRT.getDefaultState(), SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS);
-	}
+        LOGGER.info("Hello Fabric world!");
+        CustomShovelInteract.addResult(Blocks.ACACIA_PLANKS.getDefaultState(), Blocks.DIRT.getDefaultState(), SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS);
+    }
 }
--- src/main/java/net/rotgruengelb/quirl/mixin/ShovelItemMixin.java
+++ src/main/java/net/rotgruengelb/quirl/mixin/ShovelItemMixin.java
@@ -21,30 +21,30 @@ import static net.rotgruengelb.quirl.behavior.shovel.v1.CustomShovelInteract.CUS
 @Mixin(ShovelItem.class)
 public class ShovelItemMixin {
 
-	@Inject(at = @At(value = "HEAD", target = "Lnet/minecraft/item/Item;useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;"), method = "useOnBlock", cancellable = true)
-	private void extended_useOn(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
-		World world = context.getWorld();
-		BlockPos blockPos = context.getBlockPos();
-		BlockState blockState = world.getBlockState(blockPos);
-		if (context.getSide() != Direction.DOWN) {
-			PlayerEntity playerEntity = context.getPlayer();
-			ShovelInteractResult shovelInteractResult = CUSTOM_SHOVEL_INTERACT_RESULTS.get(blockState);
-			BlockState blockState3 = null;
-			if (shovelInteractResult != null && world.getBlockState(blockPos.up()).isAir()) {
-				world.playSound(playerEntity, blockPos, shovelInteractResult.getInteract_sound_event(), shovelInteractResult.getInteract_sound_category(), 1.0f, 1.0f);
-				blockState3 = shovelInteractResult.getNew_blockstate();
-			}
-			if (blockState3 != null) {
-				if (!world.isClient) {
-					world.setBlockState(blockPos, blockState3, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
-					world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, blockState3));
-					if (playerEntity != null) {
-						context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
-					}
-				}
-				cir.setReturnValue(ActionResult.success(world.isClient));
-				cir.cancel();
-			}
-		}
-	}
+    @Inject(at = @At(value = "HEAD", target = "Lnet/minecraft/item/Item;useOnBlock(Lnet/minecraft/item/ItemUsageContext;)Lnet/minecraft/util/ActionResult;"), method = "useOnBlock", cancellable = true)
+    private void extended_useOn(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        BlockState blockState = world.getBlockState(blockPos);
+        if (context.getSide() != Direction.DOWN) {
+            PlayerEntity playerEntity = context.getPlayer();
+            ShovelInteractResult shovelInteractResult = CUSTOM_SHOVEL_INTERACT_RESULTS.get(blockState);
+            BlockState blockState3 = null;
+            if (shovelInteractResult != null && world.getBlockState(blockPos.up()).isAir()) {
+                world.playSound(playerEntity, blockPos, shovelInteractResult.getInteract_sound_event(), shovelInteractResult.getInteract_sound_category(), 1.0f, 1.0f);
+                blockState3 = shovelInteractResult.getNew_blockstate();
+            }
+            if (blockState3 != null) {
+                if (!world.isClient) {
+                    world.setBlockState(blockPos, blockState3, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+                    world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, blockState3));
+                    if (playerEntity != null) {
+                        context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
+                    }
+                }
+                cir.setReturnValue(ActionResult.success(world.isClient));
+                cir.cancel();
+            }
+        }
+    }
 }
